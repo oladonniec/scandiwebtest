dac88f67c62cbb0bf9607e0d58d2628b354c2144
Lukman Olawale Oladosu <oladosul@accessbankplc.com>
2023-02-17 22:44:40 +0100

    new

--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -139,7 +139,7 @@ const AddProduct = () => {
         <h2>Product Add</h2>
         <div>
           <button type="submit" id="save-button" onClick={handleSubmit}>
-            Save
+            ADD
           </button>
           <button
             id="cancel-button"
